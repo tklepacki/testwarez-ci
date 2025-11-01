b3b5c2c26dc7a175056b05c438228d4c05de9af5
tklepacki <t.klepacki@wp.pl>
2025-11-02 00:37:33 +0100

    Add API key and headers to user requests

--- services/part_01/users.py
+++ services/part_01/users.py
@@ -1,17 +1,16 @@
 import requests
 
+API_KEY = "reqres-free-v1"
+HEADERS = {"x-api-key": API_KEY}
+
 def get_user(id):
     url = f"https://reqres.in/api/users/{id}"
-
     session = requests.session()
-    response = session.get(url)
-
+    response = session.get(url, headers=HEADERS)
     return response
 
 def get_users_list(page_id):
     url = "https://reqres.in/api/users"
-
     session = requests.session()
-    response = session.get(url, params={'page': page_id})
-
+    response = session.get(url, headers=HEADERS, params={'page': page_id})
     return response
